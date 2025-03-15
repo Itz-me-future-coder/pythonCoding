9d3ba3c12b46a204aca6fbd2593b663b4083adeb
call me sam <stsam8816@gmail.com>
2025-03-15 09:27:58 -0700

    Create MiniCalculator.py
    this is my first coding in python, I have learned in 3 hours. so I push my code using python in github

---
+++ MiniCalculator.py
@@ -0,0 +1,15 @@
+A=int(input("A:"))
+b=int(input("B:"))
+operation=input("add/sub/mul/div:")
+if(operation=="add"):
+     print(A+B)
+elif(operation=="sub"):
+     print(A-B)
+elif(operations=="mul"):
+     print(A*B)
+elif(operation=="div"):
+     print(A/B)
+else:
+    print("Invalid")
+
+ 
